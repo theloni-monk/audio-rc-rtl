061a3f52b9df75d454d494822b34989e17fd9845
Thelonious Cooper <theloni@mit.edu>
2024-12-07 14:43:18 -0500

    copied in remote build scripts

---
+++ building/r_build.sh
@@ -0,0 +1,2 @@
+cp hwbuild.py build.py
+./remote/r.py build.py build.tcl dummy_model.sv hdl/* xdc/* data/* obj
---
+++ building/r_sim.sh
@@ -0,0 +1,2 @@
+cp sbuild.py build.py
+./remote/r.py build.py xsim_run.tcl hdl/* data/* dummy_model.sv sim/generated_integration_tb_0.sv obj
