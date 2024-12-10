474ba0c5596f3df00fddb38745bf973593af9307
Theo <theo.acooper@gmail.com>
2024-12-10 18:16:16 -0500

    improved test suite

--- sim/theloni_simlib/bespoke.py
+++ sim/theloni_simlib/bespoke.py
@@ -23,15 +23,15 @@ from onnx.reference import ReferenceEvaluator
 from .ctb_util import *
 from .msgtypes import BspkVectorMsg
 
-def np2fxp(arr: np.ndarray, fmt = 'q2.6') -> List[FixedPoint]:
-    if fmt != 'q2.6':
+def np2fxp(arr: np.ndarray, fmt = 'q4.8') -> List[FixedPoint]:
+    if fmt != 'q4.8':
         raise NotImplementedError
     out = []
-    if len(arr.shape) == 1:
+    if arr.ndim == 1:
         if isinstance(arr[0], (np.float16, np.float32, np.float64)):
-            out = [FixedPoint(float(el), m=2, n=6) for el in arr]
+            out = [FixedPoint(float(el), signed=True, m=4, n=8) for el in arr]
         elif isinstance(arr[0], (np.int8, np.int16, np.int32, np.int64)):
-            out = [FixedPoint(int(el), m=2, n=6) for el in arr]
+            out = [FixedPoint(int(el), signed=True, m=4, n=8) for el in arr]
     else:
         raise NotImplementedError
 
@@ -40,6 +40,10 @@ def np2fxp(arr: np.ndarray, fmt = 'q2.6') -> List[FixedPoint]:
 def fxp2np(arr: List[FixedPoint]) -> np.array:
     return np.array([float(x) for x in arr], dtype=np.float32)
 
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
 
 class BspkDriver(BusDriver):
     def __init__(self, dut, name, clk, debug=False):
@@ -60,7 +64,7 @@ class BspkDriver(BusDriver):
         fxparr = np2fxp(msg)
         await RisingEdge(self.clock)
 
-        insig = cocotb.binary.LogicArray("".join([f"{fxp:08b}" for fxp in fxparr]))
+        insig = cocotb.binary.LogicArray("".join([f"{fxp:012b}" for fxp in fxparr]))
         if self.debug:
             print(self.name, "sending bits", insig)
         self.dut.in_data_top.value = insig
@@ -74,10 +78,10 @@ class BspkDriver(BusDriver):
         self.sent_buf.append(fxparr)
 
 class BspkFIFOMonitor(BusMonitor):
-    def __init__(self, fifo, name, clk, mon_out = True, debug=False, **kwargs):
+    def __init__(self, fifo, name, clk, mon_in = True, debug=False, **kwargs):
         self.debug = debug
         self._signals = ['wr_en', 'wr_data', 'rd_en', 'rd_data', 'wrap_rd']
-        self.mon_out = mon_out
+        self.mon_in = mon_in
         BusMonitor.__init__(self, fifo, name, clk, **kwargs)
         self.clock = clk
         self.dut = fifo
@@ -91,16 +95,17 @@ class BspkFIFOMonitor(BusMonitor):
         while True:
             await falling_edge #sometimes see in AXI shit
             await read_only  #readonly (the postline)
-            en = self.dut.wr_en if self.mon_out else self.dut.rd_en
-            data = self.dut.wr_data if self.mon_out else self.dut.rd_data
+            en = self.dut.wr_en if self.mon_in else self.dut.rd_en
+            data = self.dut.wr_data if self.mon_in else self.dut.rd_data
             if en.value:
-                bbuff = data.value.buff
-                fxpbuff = [FixedPoint(f"0b{brep:08b}", signed=True, m=2, n=6) for brep in bbuff]
+                larr = cocotb.types.LogicArray(data.value)
+                # print(list(chunks(larr.binstr, 12)))
+                fxpbuff = [FixedPoint(f"0b{lstr}", signed=True, m=4, n=8) for lstr in chunks(larr.binstr, 12)]
                 self.fxp_recvd_buf.append(fxpbuff)
                 npbuf = fxp2np(fxpbuff)
                 self.np_recvd_buf.append(npbuf)
                 if self.debug:
-                    print(self.name,"RECV:", bbuff, "=>", npbuf)
+                    print(self.name,"RECV:", list(chunks(larr.binstr, 12)), "=>", npbuf)
                 self._recv(BspkVectorMsg(npbuf, f"{self.name}_{len(self.np_recvd_buf)}_msg"))
 
 def onnx_infer(x, sess):
@@ -108,21 +113,30 @@ def onnx_infer(x, sess):
     # print("x", x, x.shape)
     return sess.run(["Y"], {"X": x})
 
+
+async def tie(s, cond, clk):
+    while True:
+        if cond.value:
+            s.value = 1
+        else:
+            s.value = 0
+        await ClockCycles(clk, 1)
+
 class BspkModuleTester(ABC):
-    def __init__(self, dut, modelpath, in_dim, scoreboard_class, debug=False):
+    def __init__(self, dut, modelpath, in_dim, board, integrate_chunksout, debug):
         self.dut = dut
         self.in_dim = in_dim
         self.debug = debug
-
+        self.integrate_chunksout = integrate_chunksout
         self.expectedbuf = []
         self.onnx_sess = ort.InferenceSession(modelpath)
-        self.model = lambda x: self.expectedbuf.append(onnx_infer(x.data, self.onnx_sess))
+        self.model = lambda x: self.expectedbuf.append((np.stack(onnx_infer(x.data, self.onnx_sess)),))
 
         self.ind = BspkDriver(dut, 'top_in_drv', dut.clk_in, debug=debug)
         self.inm = BspkFIFOMonitor(dut.v_fifo_in_top, 'top_in_mon', dut.clk_in, debug=debug, callback=self.model)
         self.outm = BspkFIFOMonitor(dut.v_fifo_out_top, 'top_out_mon', dut.clk_in, debug=debug)
 
-        self.scoreboard = scoreboard_class(self.dut, fail_immediately=False)
+        self.scoreboard = board
         self.scoreboard.add_interface(self.outm, self.expectedbuf)
 
     def _init_clks(self):
@@ -134,6 +148,8 @@ class BspkModuleTester(ABC):
         await ClockCycles(self.dut.clk_in, 500)
         self.inm.dut.rd_en.value = 0
         self.outm.dut.rd_en.value = 0
+        if self.integrate_chunksout:
+            cocotb.start_soon(tie( self.outm.dut.rd_en, self.dut.out_data_valid, self.dut.clk_in))
 
     def stop(self):
         self.inm.stop()
@@ -145,13 +161,12 @@ class BspkModuleTester(ABC):
         pass
 
     async def run_io_test(self, n_io_pairs):
+        while not self.dut.module_ready.value:
+                await ClockCycles(1)
         for _ in range(n_io_pairs - 1):
             msg = self._gen_indata()
-            if self.debug:
-                print("RUNNING IO INSTANCE ON", msg)
             self.ind.append(msg)
-            await ClockCycles(self.dut.clk_in, 1)
-
-        msg = np.zeros(self.in_dim)
+            await RisingEdge(self.dut.module_ready)
+        msg = np.zeros(self.in_dim, dtype=np.float32)
         self.ind.append(msg)
-        await ClockCycles(self.dut.clk_in, 1)
+        await RisingEdge(self.dut.module_ready)
--- sim/theloni_simlib/boards.py
+++ sim/theloni_simlib/boards.py
@@ -3,25 +3,28 @@ from .msgtypes import *
 
 
 class EpsScoreboard(Scoreboard):
-    def __init__(self, *args, epsilon=0.01, debug = False, **kwargs):
+    def __init__(self, epsilon=0.025, debug = False, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.debug = debug
         self.eps = epsilon
+        self.maxerr = 0
 
     def compare(self, got, exp, log, strict_type=True):
         if isinstance(got, AXISMessage):
             got = got.data[0]
         if isinstance(got, BspkVectorMsg):
             got = got.data
-
+        exp = exp[0][0,0]
         err = float('inf')
         tol = self.eps
         if isinstance(got, np.ndarray):
-            err = np.sum(np.abs(got-exp))
-            tol = self.eps * len(got)
+            err = np.max(np.abs(got-exp))
+            tol = self.eps
         else:
             raise NotImplementedError
+        self.maxerr = max(err, self.maxerr)
         if err <= tol:
+            # print("####### SUCCESSS ######### ")
             if self.debug:
                 try:
                     log.info(f"SCOREBOARD Received expected transaction {len(got)} bytes : {got}")
@@ -29,15 +32,16 @@ class EpsScoreboard(Scoreboard):
                     pass
         else:
             self.errors += 1
+            print(f"######## FAILURE {err} #########")
             # Try our best to print out something useful
-            if self.debug:
-                strgot, strexp = str(got), str(exp)
-                log.error(f"Received transaction {got} differed from expected output {strexp}")
-                if self._imm:
-                    assert False, (
-                    "Received transaction differed from expected "
-                    "transaction"
-                    )
+            # if self.debug:
+            strgot, strexp = str(got), str(exp)
+            log.error(f"Received transaction {got} \ndiffered from expected output {strexp}")
+            # if self._imm:
+            assert False, (
+                "Received transaction differed from expected "
+                "transaction"
+                )
 
 class DummyScorebaord(Scoreboard):
     def __init__(self, dut, reorder_depth=0, fail_immediately=True):
