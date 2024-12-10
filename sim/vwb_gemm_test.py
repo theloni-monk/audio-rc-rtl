da0dec228ca833ba937f90c27d61799bf8b72736
Theo <theo.acooper@gmail.com>
2024-12-10 18:16:44 -0500

    PASSING WOOHOO!!!

--- sim/vwb_gemm_test.py
+++ sim/vwb_gemm_test.py
@@ -1,6 +1,6 @@
 import logging
+import math
 import os
-import pathlib
 import sys
 from pathlib import Path
 
@@ -21,14 +21,17 @@ from theloni_simlib.ctb_util import *
 
 class VWBMACTester(BspkModuleTester):
     def __init__(self, dut, modelpath, in_dim, debug, **kwargs):
-        board = EpsScoreboard(0.01, debug, dut)
-        super().__init__(dut, modelpath, in_dim, board, False, debug, **kwargs)
+        board = EpsScoreboard(0.1, debug, dut)  # debug
+        super().__init__(dut, modelpath, in_dim, board, True, debug, **kwargs)
+        self.outm.mon_in = False  # monitor output and manually request read
 
     def _gen_indata(self):
-        return np.random.rand(self.in_dim).astype(dtype=np.float32)
+        # d = np.ones(self.in_dim).astype(dtype=np.float32)#np.random.rand(self.in_dim).astype(dtype=np.float32)
+        d = np.random.randn(self.in_dim).astype(np.float)
+        return d / np.max(np.abs(d))
 
 
-CYCLES = 2
+CYCLES = 8 * 11 * (int(math.ceil(math.log2(48))) + 1)
 
 
 @cocotb.test()
@@ -37,9 +40,7 @@ async def test_vwb_macc(dut):
     tlen = int(os.getenv("TLEN"))
     mdl_dim = int(os.getenv("MDIM"))
     mdl_path = os.getenv("MPATH")
-    debug = os.getenv("DEBUG") == "True"
-    if debug:
-        print("DEBUGGING", os.environ)
+    debug = bool(os.getenv("DEBUG"))
     test = VWBMACTester(dut, mdl_path, mdl_dim, debug)
     await test.startup()
 
@@ -52,6 +53,7 @@ async def test_vwb_macc(dut):
     assert (
         test.inm.stats.received_transactions == test.outm.stats.received_transactions
     ), f"Transaction Count doesn't match! :/"
+
     dut._log.info(
         f"maximum float err between recieved and expected: {test.scoreboard.maxerr}"
     )
@@ -72,12 +74,13 @@ def test_runner(svname, mdl_dim, mdl_path, tlen, debug):
 
     sources = [proj_path / "hdl" / "dummymodels" / f"{svname}_dummy_tl.sv"]
     sources.append(proj_path / "hdl" / "interface/v_fifo.sv")
-    sources.append(proj_path / "hdl" / "mlops/vwb_mac.sv")
+    sources.append(proj_path / "hdl" / "mlops/vwb_gemm.sv")
     sources.append(proj_path / "hdl" / "mlops/mac1d.sv")
     sources.append(proj_path / "hdl" / "interface/xilinx_single_port_ram_read_first.v")
+    sources.append(proj_path / "hdl" / "mlops/addertree.sv")
 
     print("\n ### SOURCES ### \n ", [s.name for s in sources], "\n")
-    build_test_args = ["-Wall"]
+    build_test_args = ["-Wimplicit"]
     parameters = {}
     sys.path.append(str(proj_path / "sim"))
     runner = get_runner(sim)
@@ -104,29 +107,24 @@ def test_runner(svname, mdl_dim, mdl_path, tlen, debug):
 def make_random_bn_onnx(in_dim, out_dim):
     X = onnxhelp.make_tensor_value_info(f"X", TensorProto.FLOAT, [1, in_dim])
     Y = onnxhelp.make_tensor_value_info(f"Y", TensorProto.FLOAT, [1, out_dim])
-    rmean = onnxhelp.make_tensor(
-        f"rmean", TensorProto.FLOAT, [in_dim], np.random.rand(in_dim)
-    )
-    rvar = onnxhelp.make_tensor(
-        f"rvar", TensorProto.FLOAT, [in_dim], np.random.rand(in_dim)
-    )
 
-    scale = onnxhelp.make_tensor(
-        f"scale", TensorProto.FLOAT, [in_dim], np.random.rand(in_dim)
+    A = onnxhelp.make_tensor(
+        f"A",
+        TensorProto.FLOAT,
+        [in_dim, out_dim],
+        # np.eye(in_dim, out_dim))
+        0.1 * np.random.randn(in_dim, out_dim),
     )
     bias = onnxhelp.make_tensor(
-        f"B", TensorProto.FLOAT, [in_dim], np.random.rand(in_dim)
+        f"B",
+        TensorProto.FLOAT,
+        [out_dim],
+        # np.ones(out_dim))
+        0.2 * np.random.randn(out_dim),
     )
 
-    bn_node = onnxhelp.make_node(
-        "BatchNormalization",
-        ["X", "scale", "B", "rmean", "rvar"],
-        ["Y"],
-        name="onnx_bn",
-    )
-    grf = onnxhelp.make_graph(
-        [bn_node], "testgraph", [X], [Y], [scale, bias, rmean, rvar]
-    )
+    gemm_node = onnxhelp.make_node("Gemm", ["X", "A", "B"], ["Y"], name="onnx_gemm")
+    grf = onnxhelp.make_graph([gemm_node], "testgraph", [X], [Y], [A, bias])
 
     opset = onnx.OperatorSetIdProto()
     opset.version = 19
@@ -139,10 +137,10 @@ def make_random_bn_onnx(in_dim, out_dim):
     return mdl
 
 
-def make_testing_tl(dim, tlname):
-    mdl = make_random_bn_onnx(dim, dim)
+def make_testing_tl(idim, odim, tlname):
+    mdl = make_random_bn_onnx(idim, odim)
     spec = svimpl.FPGASpec(120, 600_000, 2_700_000, 100_000)
-    fpga_module = parsemodel.parse_model(mdl, dim, spec)
+    fpga_module = parsemodel.parse_model(mdl, idim, spec)
     fpga_module.alloc_regs()
     fpga_module.alloc_bram("sim")
     fpga_module.tlname = tlname
@@ -152,18 +150,20 @@ def make_testing_tl(dim, tlname):
 
 
 if __name__ == "__main__":
-    tlname = "vwb_mac_dummy_tl"
-    DIM = 11
+    tlname = "vwb_gemm_dummy_tl"
+    IDIM = 48
+    ODIM = 11
     LEN = 25
     DEBUG = False
-    uut_mdl, sv = make_testing_tl(DIM, tlname)
-    onnx.save_model(uut_mdl, "sim/data/bn_mdl.onnx")
+    uut_mdl, sv = make_testing_tl(IDIM, ODIM, tlname)
+    onnx.save_model(uut_mdl, "sim/data/gemm_mdl.onnx")
     with open(f"hdl/dummymodels/{tlname}.sv", "w") as f:
         f.write(sv)
+    # exit(0)
     test_runner(
-        "vwb_mac",
-        str(DIM),
-        os.path.abspath("sim/data/bn_mdl.onnx"),
+        "vwb_gemm",
+        str(IDIM),
+        os.path.abspath("sim/data/gemm_mdl.onnx"),
         str(LEN),
         str(DEBUG),
     )
